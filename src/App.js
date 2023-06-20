2b1391d5f0054356ddfcaea45e9b1185e99f3dd9
MgrGracz <kamil.bandzwolek@gmail.com>
2023-06-20 20:21:55 +0200

    v004
    optymalizacja i interwaly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from './components/Header';
-import React, { useEffect, useState  } from 'react';
+import React, { useEffect, useState, useCallback  } from 'react';
 import { ethers } from "ethers";
+import axios from 'axios';
 import { nftContractAddress, checkIDchain } from './config.js'
 import NFT from './abis/Bountyloop.json';
 import './styles/App.css';
-import logo1 from './assets/koszulka.jpeg';
-import logo2 from './assets/logo.png';
+import logo2 from './assets/logo.jpeg';
 
 
 const App = () => {
@@ -17,15 +17,23 @@ const App = () => {
   const [param, setParam] = useState('')
   const [kwota, setKwota] = useState('')
   const [paramSKU, setParamSKU] = useState('')
-  const [countCamp, setCountCamp] = useState(0)
+  const [kurs, setKurs] = useState(0)
+  const [zdjecieProduktu, setZdjecieProduktu] = useState(0)
   const [payStatus, setPayStatus] = useState(0)
   const [currentAccount, setCurrentAccount] = useState('')
+  const [zaplacone, setZaplacone] = useState(false)
   const [correctNetwork, setCorrectNetwork] = useState(false)
 
   
+  const data = {
+    products: [
+      { id: 1, symbol: "ERO921", name: "PGNiG", price: 67.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FD8lkaKyQoQzx45zdqcxX.jpg" },
+      { id: 2, symbol: "ERO-CC1", name:"NeXT Blue", price: 57.90, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FVDE90bjxNj9p5Pk0BiCI.jpg" },
+    ],
+  };
+
+
   console.log("-------------USE STATE--------------");
-
-
   // Calls Metamask to connect wallet on clicking Connect Wallet button 
   const connectWallet = async () => {
       try {
@@ -36,24 +44,47 @@ const App = () => {
           }
 
           await ethereum.request({
+            
             method: 'wallet_switchEthereumChain',
             params: [{ chainId: checkIDchain }],
          })
           const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
           console.log('Found account', accounts[0])
           setCurrentAccount(accounts[0])
-          checkIfWalletIsConnected();
+                  const queryParams = new URLSearchParams(window.location.search);
+                  const campID = queryParams.get('campID');
+                  const skuID = queryParams.get('SKU');
+                  const findSymbol = data.products.find(product => product.symbol === skuID);
+                  const cenaValue = findSymbol.price;
+                  setZdjecieProduktu(findSymbol.url)
+                  setKwota(cenaValue);
+
+                  setParam(campID);
+                  setParamSKU(skuID);
+                  checkIfWalletIsConnected();
+                   
       } catch (error) {
           console.log('Error connecting to metamask', error)
       }
   }
 
 
+  const getKursETH = useCallback(() => {
+    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=pln')
+      .then(response => {
+        const ethPricePLN = response.data.ethereum.pln;
+        let ethPriceETHPLN = (kwota / ethPricePLN).toFixed(4);
+        if (ethPriceETHPLN !== "") {
+          setKurs(`${ethPriceETHPLN}`);
+        }
+      });
+  }, [kwota]);
+
 
 
 
         // Checks if wallet is connected
-    const checkIfWalletIsConnected = async () => {
+    const checkIfWalletIsConnected = useCallback (async () => {
           const { ethereum } = window
           if (ethereum) {
               console.log('Got the ethereum obejct: ', ethereum)
@@ -77,34 +108,21 @@ const App = () => {
                   const signer = provider.getSigner()
                   const nftContract = new ethers.Contract(nftContractAddress, NFT, signer)
                   const queryParams = new URLSearchParams(window.location.search);
+                  
                   const campID = queryParams.get('campID');
                   const skuID = queryParams.get('SKU');
-                  const cenaId = queryParams.get('m');
-                  let cenaValue = cenaId.replace("A","1")
-                  cenaValue = cenaValue.replace("B","2")
-                  cenaValue = cenaValue.replace("C","3")
-                  cenaValue = cenaValue.replace("D","4")
-                  cenaValue = cenaValue.replace("E","5")
-                  cenaValue = cenaValue.replace("F","6")
-                  cenaValue = cenaValue.replace("G","7")
-                  cenaValue = cenaValue.replace("H","8")
-                  cenaValue = cenaValue.replace("I","9")
-                  cenaValue = cenaValue.replace("J","0")
-                  cenaValue = cenaValue.replace("K",",")
+                  const findSymbol = data.products.find(product => product.symbol === skuID.toString());
+                  const cenaValue = findSymbol.price;
+                  setZdjecieProduktu(findSymbol.url)
                   setKwota(cenaValue)
+                  console.log(campID)
                   setParam(campID);
                   setParamSKU(skuID);
                   const techCamp = await nftContract.getCampaign(campID)
-                  setCamp(techCamp)
-                  
-                  let techCount = await nftContract._campaignCount()
-                  setCountCamp(techCount.toString())
-                
-
-
+                  setCamp(techCamp) 
               } else {
                   console.log('No authorized account found')
-
+                  //setCurrentAccount("");
               }
 
           } else {
@@ -112,24 +130,37 @@ const App = () => {
               setmetamaskOwner(false);
           }
     
-      }
+      },[data.products])
 
   //setInterval(checkIfWalletIsConnected, 5000);
 
   
-  useEffect(() => {
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        const isZaplacone = localStorage.getItem('zaplacone');
+        if (isZaplacone) {
+          setZaplacone(true);
+        }
       checkIfWalletIsConnected();
-  }, [])
+      getKursETH();
+    }, 5000);
+  
+    return () => {
+      clearInterval(interval);
+    };
+  }, [checkIfWalletIsConnected, getKursETH]);
 
     const pay = async () => {
       try {
+        setTxError("")
           const { ethereum } = window
           if (ethereum) {
           console.log("pay");
           const provider = new ethers.providers.Web3Provider(ethereum)
           const signer = provider.getSigner()
           const nftContract = new ethers.Contract(nftContractAddress, NFT, signer)
-          let money= ethers.utils.parseEther("0.01"); 
+          let money= ethers.utils.parseEther(kurs.toString());
           console.log('Platnosc....'+ money)
           let nftTx = await nftContract.donateToCampaign(param, { value: money});
           
@@ -137,7 +168,8 @@ const App = () => {
           
           setPayStatus(1)
           let tx = await nftTx.wait()
-
+          setZaplacone(true)
+          localStorage.setItem('zaplacone', true);
           setPayStatus(0)
 
           console.log('Zaplacono!', tx)
@@ -146,19 +178,21 @@ const App = () => {
               console.log("Ethereum object doesn't exist!")
           }
       } catch (error) {
-          console.log('Error minting character', error)
+          console.log('Error pay', error)
           setTxError(error.message)
           
       }
       
   }
 
+  
+
 //########################################################
 //FRONT
 
 const renderNotConnectedContainer = () => (
     <div id="buttonGrid">
-        <button onClick={connectWallet} class="button pulse" >
+        <button onClick={connectWallet} className="button pulse" >
         Połącz portfel
       </button>
 </div>
@@ -167,16 +201,32 @@ const renderNotConnectedContainer = () => (
 const renderMintUI = () => (  
 <div>
 <div id="logoTextCenter">
-                <p><b>{kwota} PLN</b></p> 
+                <p>{kwota} PLN</p>
+                <p>{kurs} ETH</p> 
         </div> 
-  <div id="buttonGrid"> 
-  <button href="#" class="button pulse" onClick={pay}>
+        
+  {payStatus === 1 ? (
+
+<div id="buttonGrid"> 
+  <button href="#" className="button block">
+  <div className="wrapper">
+  <span className="circle circle-1"></span>
+  <span className="circle circle-2"></span>
+  <span className="circle circle-3"></span>
+  <span className="circle circle-4"></span>
+  <span className="circle circle-5"></span>
+  <span className="circle circle-6"></span>
+</div>
+Trwa płatność
+  </button>
+</div>)
+: ( 
+<div id="buttonGrid"> 
+  <button href="#" className="button pulse" onClick={pay}>
     Zapłać
   </button>
-</div>
-<div id="errorMSG">
-    <p>Tym zakupem wspierasz akcje charytatywne z kategorii: {camp.title}</p>
-    </div>
+</div>)}
+
 </div>
 );
 
@@ -188,11 +238,13 @@ const campValue = () => (
         <div id="logoText">
                 <p>Produkt</p> 
                 <p><b>{paramSKU}</b></p> 
+                <p>Kategoria</p>
+                <p><b>{camp.title}</b></p>
         </div> 
     </div>
     <div id="bodySlotGrid">
-        <div class="okragla-kontrolka">
-            <img src={logo1} alt="Zdjęcie" />
+        <div className="okragla-kontrolka">
+            <img src={zdjecieProduktu} alt="Zdjęcie" />
         </div>
     </div>
     <div id="bodySlotGrid">
@@ -202,6 +254,18 @@ const campValue = () => (
   </div>
   );
 
+
+const zaplaconoView =() => (
+    <div>
+        <div id="exitView">
+        <img id="connectImg" src={logo2} alt="Connect2" />
+            <div id="introView">
+                Produkt został poprawnie opłacony!
+            </div>
+
+            </div>
+    </div>
+)
 
   const noLogView= () => (    
     <div>
@@ -222,14 +286,13 @@ const campValue = () => (
 
       <Header currentAccount={currentAccount} />   
       
-      {metamaskOwner === true ? ( currentAccount === "" ? noLogView() : campValue() ) : ""}
+      {metamaskOwner === true ? ( currentAccount === "" ? noLogView() : (   zaplacone ===true ? zaplaconoView() : campValue()) ) : ""}
       {correctNetwork === false &&  currentAccount !== ""? <div id="buttonGrid2"><div id="labelMint">ERROR CHAIN - Change chain to Ethereum</div></div> : ""} 
       <div>
-      {metamaskOwner === true ? ( currentAccount === "" ? renderNotConnectedContainer() : renderMintUI() ) : <div id="metamaskEmpty" /> }
+      {metamaskOwner === true ? ( currentAccount === "" ? renderNotConnectedContainer() : (zaplacone === false ? renderMintUI() : "") ) : <div id="metamaskEmpty" /> }
       </div>
       <div id="errorMSG">{txError}</div> 
 
-      {payStatus === 1 ? <div id="errorMSG">TRWA PLATNOSC</div> : ""}
       </div>
     );
   
--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-export const nftContractAddress = "0x5F6045e852A6F05C8Cf00Cff1C04b4Cb54301B3C"; 
-export const boolDontMint = false;
-export const checkIDchain = '0x5';   //GOERLI    '0x5 '   mainnet  '0x1'    Sepolia '0x46'
-export const mintDateTimer = "";   // Jak jest 0 to nie pokazuje '02 19, 2023 20:50:00' 
+export const nftContractAddress = "0x7bd6A97a7454b9D331FD7d9Cc3D6cbCB67E4F453"; 
+export const checkIDchain = '0xaa36a7';   //GOERLI    '0x5 '   mainnet  '0x1'    Sepolia '0x46'
+  // Jak jest 0 to nie pokazuje '02 19, 2023 20:50:00' 
