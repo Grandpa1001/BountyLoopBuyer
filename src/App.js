41ad8edc97ca1349b0386f6062e366a4c5c08304
MgrGracz <kamil.bandzwolek@gmail.com>
2023-06-23 01:02:13 +0200

    v006
    go

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
   const [camp, setCamp] = useState('')
   const [param, setParam] = useState('')
   const [kwota, setKwota] = useState('')
+  const [nazwaProduktu, setNazwaProduktu] = useState('')
   const [paramSKU, setParamSKU] = useState('')
   const [kurs, setKurs] = useState(0)
   const [zdjecieProduktu, setZdjecieProduktu] = useState(0)
@@ -29,6 +30,21 @@ const App = () => {
     products: [
       { id: 1, symbol: "ERO921", name: "PGNiG", price: 67.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FD8lkaKyQoQzx45zdqcxX.jpg" },
       { id: 2, symbol: "ERO-CC1", name:"NeXT Blue", price: 57.90, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FVDE90bjxNj9p5Pk0BiCI.jpg" },
+      { id: 3, symbol: "SHEI01", name:"Shein Wolf", price: 48.50, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FkXSOKSmRL1RNLbULCe1j.jpeg"},
+      { id: 4, symbol: "BLANK01", name:"Casula white", price: 19.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FnsAJz2ymYqLI7aUoML6w.webp"},
+      { id: 5, symbol: "GAME01", name:"GamePad", price: 24.20, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2F9ri0kDsXzKtNDHUpkuce.jpeg" },
+      { id: 6, symbol: "SHEIP03", name:"Shein Pack black", price: 72.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FSEePFzoXHfAR3JuQT8MK.jpeg" },
+      { id: 7, symbol: "HAEM01", name:"H&M sukienka green", price: 89.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FfIDw9UBoFDCQ93d6zvRV.jpeg" },
+      { id: 8, symbol: "HAEM02", name:"H&M sukienka czarna", price: 89.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FxaQsDfALbPZGCPv3HsOw.jpeg" },
+      { id: 9, symbol: "HAEM03", name:"H&M bluzka wyszywana", price: 99.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FbG7DgiJJchssJg9BcPoH.jpeg" },
+      { id: 10, symbol: "HAEM04", name:"Falbaniasta spódnica mini", price: 79.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2Fk3W4Y4qxldrVaaM7RMEC.jpeg" },
+      { id: 11, symbol: "HAEM05", name:"Koszulka polo", price: 100.00, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2Fw3t8LvIHjZOkBUc1KZE1.jpeg" },
+      { id: 12, symbol: "BLNO2", name:"Koszulka biała zalando", price: 45.40, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FzwGsQbyIfPEgiHMolPTz.webp" },
+      { id: 13, symbol: "BLN03", name:"Koszulka plaza biała", price: 52.20, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FIcYiERKM8cSkvpx5WaYE.jpeg" },
+      { id: 14, symbol: "SPOD01", name:"Spodnie białe", price: 129.12, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2ForNYeTuOZFyhdvR3AXcT.jpeg" },
+      { id: 15, symbol: "BLUZ04", name:"Bluza zielona", price: 89.99, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FfIDw9UBoFDCQ93d6zvRV.jpeg" },
+      { id: 16, symbol: "BLUZ05", name:"Bluza zielona niebieska", price: 76.20 , url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2FXBXryEq082PtBdz5QZM4.jpeg" },
+      { id: 17, symbol: "KOSZ92", name:"Koszula kolorowa", price: 92.10, url: "https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fstorage.googleapis.com%2Fglide-prod.appspot.com%2Fuploads-v2%2FE2f4NOk6eUqYE7KZAnNM%2Fpub%2F7TXXUSsmK1VJ8buyO3k7.jpeg" },
     ],
   };
 
@@ -112,6 +128,7 @@ const App = () => {
                   const findSymbol = data.products.find(product => product.symbol === skuID.toString());
                   const cenaValue = findSymbol.price;
                   setZdjecieProduktu(findSymbol.url)
+                  setNazwaProduktu(findSymbol.name)
                   setKwota(cenaValue)
                   console.log(campID)
                   setParam(campID);
@@ -199,7 +216,9 @@ const renderNotConnectedContainer = () => (
 
 const renderMintUI = () => (  
 <div>
+  
 <div id="logoTextCenter">
+<p><b>Cena</b></p> 
                 <p>{kwota} PLN</p>
                 <p>{kurs} ETH</p> 
         </div> 
@@ -220,11 +239,13 @@ Trwa płatność
   </button>
 </div>)
 : ( 
+  <div>
 <div id="buttonGrid"> 
   <button href="#" className="button pulse" onClick={pay}>
     Zapłać
   </button>
-</div>)}
+</div>
+<div id="dopisek">Zakupem wspierasz kategorie: <b>{camp.title}</b></div></div>)}
 
 </div>
 );
@@ -235,10 +256,8 @@ const campValue = () => (
     <div id="Body">
     <div id="bodySlotGrid">
         <div id="logoText">
-                <p>Produkt</p> 
-                <p><b>{paramSKU}</b></p> 
-                <p>Kategoria</p>
-                <p><b>{camp.title}</b></p>
+                <div id="logoTextTitle"><p>Produkt</p></div> 
+                <p><b>{nazwaProduktu}</b></p> 
         </div> 
     </div>
     <div id="bodySlotGrid">
@@ -269,12 +288,12 @@ const zaplaconoView =() => (
   const noLogView= () => (    
     <div>
         <div id="introGrid">
-            <div id="introView">
-                Witaj na stronie BountyLoop Pay
-            </div>
+        <img id="introImgView" src={logo2} alt="Connect2" />
         </div>
         <div id="introGrid">
-        <img id="introImgView" src={logo2} alt="Connect2" />
+            <div id="introView">
+                Witaj na stronie BountyLoop Pay, by przejść dalej
+            </div>
         </div>
   </div>
   );
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ root.render(
   <React.StrictMode>
     <App />
     <div className="sticky-footer">
-    Powered by <a href="https://bountyloop.com" className="sticky-link" target="_blank" rel="noopener noreferrer">BountyLoop</a>
+    Powered by <a href="https://bountyloop.glide.page/" className="sticky-link" target="_blank" rel="noopener noreferrer">BountyLoop</a>
     </div>
   </React.StrictMode>
 );
