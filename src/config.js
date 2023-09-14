2bfb1fa58b901b88a07c095633a9e94e61b395c5
MgrGracz <kamil.bandzwolek@gmail.com>
2023-09-14 17:46:04 +0200

    update contract

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const nftContractAddress = "0x2f95476246BC3582eDA860892293563F866409DE"; 
+export const nftContractAddress = "0x035a32B25F33B7925d28150511fBeCB0b5667895"; 
 export const checkIDchain = '0xaa36a7';   //GOERLI    '0x5 '   mainnet  '0x1'    Sepolia '0x46'
   // Jak jest 0 to nie pokazuje '02 19, 2023 20:50:00' 
